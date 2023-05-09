e9d2274da34cfa474e58ad176ed5502207644061
michel-macuacua <michelfernandes802@gmail.com>
2023-05-09 12:29:12 +0200

    Tratamento de erro 404

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,11 @@
-import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 import { Course } from '../models/course';
-import { Observable } from 'rxjs';
+import { CoursesService } from './../services/courses.service';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -13,8 +17,23 @@ export class CoursesComponent implements OnInit {
   courses$: Observable <Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+  constructor(private coursesService: CoursesService,
+    public dialog: MatDialog) {
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(
+        error => {
+          this.onError('Erro ao carregar cursos.');
+          return of([])
+        }
+      )
+    );
+  }
+
+  onError(errorMsg: string){
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
