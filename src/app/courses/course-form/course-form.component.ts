25fde09924d387014c2c670c5a8f22635bfed1c5
michel-macuacua <michelfernandes802@gmail.com>
2023-05-10 15:14:24 +0200

    [CRUD] - Creation & List with Spring API

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
+
 
 import { CoursesService } from '../services/courses.service';
 
@@ -15,7 +17,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar) {
+    private snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -27,12 +30,19 @@ export class CourseFormComponent implements OnInit {
   ngOnInit(): void {}
 
   onSubmit(){
-    this.service.save(this.form.value).subscribe(result => console.log(result), error =>   this.onError());
+    this.service.save(this.form.value).subscribe(result => this.onSuccess(), error =>   this.onError());
   }
 
-  onCancel(){}
+  onCancel(){
+    this.location.back();
+  }
 
   private onError(){
     this.snackBar.open('Erro ao salvar curso', '', {duration: 1000})
+  }
+
+  private onSuccess(){
+    this.snackBar.open('Curso salvo com sucesso!!', '', {duration: 5000})
+    this.onCancel();
   }
 }
